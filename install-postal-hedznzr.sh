efe070a706455c5126f255be7421edaa97cd3b6e
mrsbaai <abdelilah.sbaai@gmail.com>
2020-10-15 16:23:13 +0100

    Update install-postal-hedznzr.sh

--- install-postal-hedznzr.sh
+++ install-postal-hedznzr.sh
@@ -118,7 +118,7 @@ service nginx reload
 
 
 cd /etc/systemd/system;
-curl -O https://raw.githubusercontent.com/mrsbaai/master/postal.service;
+curl -O https://raw.githubusercontent.com/mrsbaai/postal.service;
 systemctl daemon-reload;
 systemctl enable postal;
 systemctl start postal;
